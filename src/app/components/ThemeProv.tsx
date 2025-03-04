ad18399120cffe862e42c2c5c5a4e96bc5075b36
antonis <antwnis_skarlatos@yahoo.com>
2025-03-04 15:17:09 +0200

    dark mode

---
+++ src/app/components/ThemeProv.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+
+export default function ThemeProv({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system">
+      <div className="min-h-screen select-none transition-colors duration-300">
+        {children}
+      </div>
+    </ThemeProvider>
+  );
+}
